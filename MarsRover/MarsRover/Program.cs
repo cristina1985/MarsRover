2f3b9fd0225c3c24502b36da48ee97479526819b
Cristina <cristinamoraru7@yahoo.com>
2020-05-13 17:32:23 +0300

    step2

--- MarsRover/MarsRover/Program.cs
+++ MarsRover/MarsRover/Program.cs
@@ -6,7 +6,23 @@ namespace MarsRover
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[ , ] index = new int[15,15] ;
+            Console.WriteLine("Please provide the rover first coordinate ( must be <=15 ) :");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please provide the rover second coordinate ( must be <=15 ) :");
+            int y = int.Parse(Console.ReadLine());
+            index[x, y] = 5;
+            int i, j;
+            for (i=0; i<15; i++)
+            {
+                for (j=0; j<15; j++)
+                {
+                    Console.WriteLine("index[{0},{1}] = {2}",i,j, index[i, j]);
+                }
+                
+            }
+            
+            
         }
     }
 }
